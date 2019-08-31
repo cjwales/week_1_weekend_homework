45a134c859744c85d6950df85ee2cb1591cd6f51
cjwales <cjwales@gmail.com>
2019-08-31 11:43:53 +0100

    finished MVP

---
+++ pet_shop.rb
@@ -0,0 +1,78 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
+  return cash
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, number_sold)
+  pet_shop[:admin][:pets_sold] += number_sold
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length()
+end
+
+def pets_by_breed(pet_shop, breed)
+  desired_breed = []
+
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      desired_breed.push(pet)
+    end
+  end
+  return desired_breed
+end
+
+def find_pet_by_name(pet_shop, name)
+  specified_name = []
+
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet.shift()
+    end
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+def customer_cash(customers)
+  for person in customers
+    return customers[:cash]
+  end
+end
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+end
+
+def customer_pet_count(customers)
+  for person in customers
+    return customers[:pets].count()
+  end
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
